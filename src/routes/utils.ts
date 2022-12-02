925134d21b48669987541f739803ed3adb6c2ece
Elson Ho <elsonho.ws@gmail.com>
2022-12-02 16:50:30 +0800

    change prefix to from apps to frontend-application

--- src/routes/utils.ts
+++ src/routes/utils.ts
@@ -1,3 +1,3 @@
 /* This application will be hosted behind a proxy. Added '/apps' prefix to prevent route name clashing with proxy name */
-const PATH_PREFIX = '/apps';
+const PATH_PREFIX = '/frontend-application';
 export const generateAppPath = (path) => `${PATH_PREFIX}${path}`;
